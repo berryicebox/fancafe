d23af9efc36802e0103abf9e121f6f536d65fad9
sosojean <thwlsdl0989@gmail.com>
2024-10-31 17:09:56 +0900

    design: 화면요소 너비에 맞게 배치
    width 1080px 전체 적용
    setting css 변수 값 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import "../assets/styles/header.scss";
 // import "../assets/styles/setting.css";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import {useEffect, useState} from "react";
 
-const Header = ({ props }) => {
+const Header = ({props}) => {
 
     const [isAuth, setIsAuth] = useState(false)
 
@@ -12,28 +12,34 @@ const Header = ({ props }) => {
             setIsAuth(true)
         }
     })
-    const handleLogout = () =>{
+    const handleLogout = () => {
         localStorage.removeItem('accessToken');
         setIsAuth(false)
     }
 
     return (
-            <div className="headerContainer">
+        <div className="headerContainer">
+            <div className="headerContentsContainer">
                 <div className="headerLnCContainer">
-                <Link to="/"><h1>FanCafe</h1></Link>
+                    <Link to="/"><h1>FanCafe</h1></Link>
                     <ul className="headerCatecoryContainer">
-                        <Link to="/best"><li>인기글</li></Link>
-                        <Link to="/new"><li>전체글</li></Link>
+                        <Link to="/best">
+                            <li>인기글</li>
+                        </Link>
+                        <Link to="/new">
+                            <li>전체글</li>
+                        </Link>
                     </ul>
                 </div>
                 <div className="headerRegisterContainer">
                     {isAuth ?
                         <Link onClick={handleLogout}>로그아웃</Link>
-                        :<><Link to="/join">회원가입</Link>
-                         <Link to="/login">로그인</Link></>}
+                        : <><Link to="/join">회원가입</Link>
+                            <Link to="/login">로그인</Link></>}
 
                 </div>
             </div>
+        </div>
     )
 }
 
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,7 +1,6 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import App from "../App";
 import Home from "../pages/Home";
 import Test from "./Test";
 import ItemDetail from "./ItemDetail";
@@ -12,29 +11,36 @@ import Login from "../pages/Login";
 import WriteButton from "./WriteButton";
 import PostWrite from "../pages/PostWrite";
 import JoinSuccess from "../pages/JoinSuccess";
+import "../assets/styles/router.scss";
 
-const Router = ( props ) => {
+const Router = (props) => {
     console.log(props);
     return (
-        <div>
-            <BrowserRouter>
+        <BrowserRouter>
+
+            <div className="outer-container">
                 <Header/>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/item/:itemId" element={<ItemDetail />} />
-                    <Route path="/test" element={<Test />} /> {/*라우터 사용 예시*/}
-                    <Route path="/best" element={<Best />} />
-                    <Route path="/new" element={<New />} />
-                    <Route path="/join" element={<Join />} />
-                    <Route path="/login" element={<Login reload = {props.reload} setReload = {props.setReload} />} />
-                    <Route path="/post/write" element={<PostWrite/>}/>
-                    <Route path="/join/success" element={<JoinSuccess />}/>
-                    <Route path="/login" element={<Login />} />
-                </Routes>
+                <div className="inner-container">
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/item/:itemId" element={<ItemDetail/>}/>
+                        <Route path="/test" element={<Test/>}/> {/*라우터 사용 예시*/}
+                        <Route path="/best" element={<Best/>}/>
+                        <Route path="/new" element={<New/>}/>
+                        <Route path="/join" element={<Join/>}/>
+                        <Route path="/login" element={<Login reload={props.reload} setReload={props.setReload}/>}/>
+                        <Route path="/post/write" element={<PostWrite/>}/>
+                        <Route path="/join/success" element={<JoinSuccess/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                    </Routes>
+                </div>
+
                 <WriteButton/>
                 <Footer/>
-            </BrowserRouter>
-        </div>
+
+            </div>
+        </BrowserRouter>
+
     )
 }
 
