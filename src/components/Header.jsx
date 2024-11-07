4999695175b6171b9ab2f110f9f13e58650da04f
sosojean <thwlsdl0989@gmail.com>
2024-11-07 19:10:05 +0900

    feat: 카테고리 생성
    카테고리 생성
    글쓰기 value 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import "../assets/styles/header.scss";
 import {Link, useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
+import Category from "../pages/Category";
+import {faChevronDown} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Header = (props) => {
 
     const [isAuth, setIsAuth] = useState(false)
     const [keyword, setKeyword] = useState()
+    const [hover, setHover] = useState(false)
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -29,44 +33,65 @@ const Header = (props) => {
 
     }
 
-    return (
-        <div className="headerContainer">
-            <div className="headerContentsContainer">
-                <div className="headerLnCContainer">
-                    <Link to="/"><h1>FanCafe</h1></Link>
-                    <ul className="headerCatecoryContainer">
-                        <Link to="/best">
-                            <li>인기글</li>
-                        </Link>
-                        <Link to="/new">
-                            <li>전체글</li>
-                        </Link>
-                    </ul>
+    const hoverCategory = () => {
+        console.log("hoverCategory");
+        setHover(true);
+    }
+
+    const leaveHeader = () => {
+        setHover(false);
+
+    }
+
+    return (<>
+            <div className="headerContainer" onMouseLeave={leaveHeader}>
+                <div className="headerContainer">
+                    <div className="headerContentsContainer">
+                        <div className="headerLnCContainer">
+                            <Link to="/"><h1 onMouseEnter={leaveHeader}>FanCafe</h1></Link>
+                            <ul className="headerCatecoryContainer">
+                                <Link onMouseEnter={leaveHeader} to="/best">
+                                    <li>인기글</li>
+                                </Link>
+                                <Link onMouseEnter={leaveHeader} to="/new">
+                                    <li>전체글</li>
+                                </Link>
+                                <Link onMouseEnter={hoverCategory} className="category">
+                                    <li>카테고리 <FontAwesomeIcon icon={faChevronDown}/></li>
+                                </Link>
+                            </ul>
+                        </div>
+
+                        <form onMouseEnter={leaveHeader} className="searchBar" action="/search"
+                              onSubmit={(e) => {
+                                  searchHandler(e)
+                              }}>
+                            <input id="searchInput" type="text"
+                                   name="keyword" maxLength="50"
+                                   placeholder="글 제목, 본문 검색"
+                                   onChange={(e) => {
+                                       setKeyword(e.target.value)
+                                   }}/>
+                            {/*<button onClick={searchHandler}></button>*/}
+                        </form>
+
+                        <div className="headerRegisterContainer" onMouseEnter={leaveHeader}>
+                            {isAuth ? <>
+                                    <Link to="/mypage">마이페이지</Link>
+                                    <Link onClick={handleLogout}>로그아웃</Link></>
+                                : <><Link to="/join">회원가입</Link>
+                                    <Link to="/login">로그인</Link></>}
+
+                        </div>
+                    </div>
+
                 </div>
+                {hover ? <Category></Category> : null}
 
-                <form className="searchBar" action="/search"
-                      onSubmit={(e) => {
-                          searchHandler(e)
-                      }}>
-                    <input id="searchInput" type="text"
-                           name="keyword" maxLength="50"
-                           placeholder="글 제목, 본문 검색"
-                           onChange={(e) => {
-                               setKeyword(e.target.value)
-                           }}/>
-                    {/*<button onClick={searchHandler}></button>*/}
-                </form>
-
-                <div className="headerRegisterContainer">
-                    {isAuth ? <>
-                            <Link to="/mypage">마이페이지</Link>
-                            <Link onClick={handleLogout}>로그아웃</Link></>
-                        : <><Link to="/join">회원가입</Link>
-                            <Link to="/login">로그인</Link></>}
-
-                </div>
             </div>
-        </div>
+            {hover ? <div className="background-blocker"></div> : null}
+        </>
+
     )
 }
 
--- src/pages/PostWrite.jsx
+++ src/pages/PostWrite.jsx
@@ -1,21 +1,21 @@
-import { useCallback, useRef, useState } from "react"
+import {useCallback, useRef, useState} from "react"
 import WriteEditor from "../components/WriteEditor"
 import '../assets/styles/postWrite.scss'
-import { Link, useNavigate } from "react-router-dom";
+import {Link} from "react-router-dom";
 import instance from "../components/axios";
 
 
-export default function PostWrite(){
+export default function PostWrite() {
     // 작성 내용(contents), 제목(title), 카테고리(category) 저장
     const editorRef = useRef(null);
     const [title, setTitle] = useState('');
     // 카테고리 선택 최상단에 자유가 있으므로 디폴트 값으로 자유
     const [category, setCategory] = useState('자유');
-  
+
     // 카테고리 선택할 때 저장
-    function onSelectCategory(e){
-    //   console.log(e.target.value); // 테스트 코드
-      setCategory(e.target.value);
+    function onSelectCategory(e) {
+        //   console.log(e.target.value); // 테스트 코드
+        setCategory(e.target.value);
     }
 
     // 제출할 때 db에 post
@@ -25,22 +25,21 @@ export default function PostWrite(){
         // navigate 사용시 button의 내용이 일시적으로 사라지는 문제 발생 (추후에 해결해볼게요)
         if (!markdown) {
             alert('내용을 작성해주세요.');
-            return;
-        }
-        else {
+
+        } else {
             const token = localStorage.getItem('accessToken'); // 토큰 가져오기
-    
+
             // console.log('token: ', token); // 테스트 코드
-    
+
             // 보낼 데이터
             const data = {
-                "title" : title, 
-                "contents" : markdown, 
-                "category" : category
+                "title": title,
+                "contents": markdown,
+                "category": category
             }
-    
+
             console.log('sending data: ', data); // 테스트 코드
-    
+
             // axios + refresh 토큰 필요 유무 확인 메서드
             instance({
                 method: "POST",
@@ -55,28 +54,29 @@ export default function PostWrite(){
         }
     }, [category, title]);
 
-    return(
+    return (
         <div>
-                <select name='category' value={category}
+            <select name='category' value={category}
                     onChange={(e) => onSelectCategory(e)}
                     className="selectorCategory">
-                    <option value='자유'>자유</option>
-                    <option value='강사님께 질문'>강사님께 질문</option>
-                    <option value='강사님 OOTD'>강사님 OOTD</option>
-                    <option value='강사님 팬아트'>강사님 팬아트</option>
-                    <option value='강사님께 한마디'>강사님께 한마디</option>
-                    <option value='졸업생 커뮤니티'>졸업생 커뮤니티</option>
-                    <option value='데일리 코테'>데일리 코테</option>
-                </select>
-                <input placeholder='제목' name='title'
-                    className="inputTitle"
-                    onChange={(e) => setTitle(e.target.value)}
-                />  
-                <WriteEditor ref={editorRef}/>
-                <Link to="/">
-                    <button onClick={onClickEnrollBtn}
-                        className="submitButton">등록</button>
-                </Link>
+                <option value='free'>자유</option>
+                <option value='question'>강사님께 질문</option>
+                <option value='ootd'>강사님 OOTD</option>
+                <option value='fanart'>강사님 팬아트</option>
+                <option value='comment'>강사님께 한마디</option>
+                <option value='graduate'>졸업생 커뮤니티</option>
+                <option value='daily'>데일리 코테</option>
+            </select>
+            <input placeholder='제목' name='title'
+                   className="inputTitle"
+                   onChange={(e) => setTitle(e.target.value)}
+            />
+            <WriteEditor ref={editorRef}/>
+            <Link to="/">
+                <button onClick={onClickEnrollBtn}
+                        className="submitButton">등록
+                </button>
+            </Link>
         </div>
     )
 }
