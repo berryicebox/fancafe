8956056c037ed59c3ab9c3481d5a90eaffd24a06
sosojean <thwlsdl0989@gmail.com>
2024-11-07 09:21:53 +0900

    chore: 라우터 임포트 제거
    미사용 컴포넌트 라우터 제거

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,8 +2,6 @@ import Header from "./Header";
 import Footer from "./Footer";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "../pages/Home";
-import Test from "./Test";
-import ItemDetail from "./ItemDetail";
 import Best from "../pages/Best";
 import New from "../pages/New";
 import Join from "../pages/Join";
@@ -24,8 +22,8 @@ const Router = (props) => {
                 <div className="inner-container">
                     <Routes>
                         <Route path="/" element={<Home isAuth={props.isAuth}/>}/>
-                        <Route path="/item/:itemId" element={<ItemDetail/>}/>
-                        <Route path="/test" element={<Test/>}/> {/*라우터 사용 예시*/}
+                        {/*<Route path="/item/:itemId" element={<ItemDetail/>}/>*/}
+                        {/*<Route path="/test" element={<Test/>}/> /!*라우터 사용 예시*!/*/}
                         <Route path="/best" element={<Best/>}/>
                         <Route path="/new" element={<New/>}/>
                         <Route path="/join" element={<Join/>}/>
