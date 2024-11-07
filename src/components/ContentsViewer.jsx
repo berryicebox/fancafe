dd3e242078190dceafe66c44512de7ad159c837b
berryicebox <feelmypasta@gmail.com>
2024-11-07 18:24:02 +0900

    Revert "feat: 시간 n시간전 등으로 표시 (커스텀 훅 생성)"
    This reverts commit 79501abc05e984a9b3c843311747dbb4e21d28fb.

--- src/components/ContentsViewer.jsx
+++ src/components/ContentsViewer.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import instance from './axios.jsx'
+<<<<<<< HEAD
 import { Viewer } from '@toast-ui/react-editor';
 import HeartButton from './HeartButton.jsx';
-import { useModifyTime } from '../utils/useModifyTime.jsx';
+=======
+
+import {Viewer} from '@toast-ui/react-editor';
+
+import Comments from "./Comments";
+
+>>>>>>> b8d70a19c06c054c3dce51058c651bf24c654e0d
 
 const ContentsViewer = ({props}) => {
     const { category, post_id } = useParams();
     const [contentInfo, setContentInfo] = useState(null);
-    const modifiedTime = useModifyTime(contentInfo?.createdDate);
+
 
     useEffect(() => {
         instance({
@@ -25,6 +32,14 @@ const ContentsViewer = ({props}) => {
         return (<p>데이터가 없습니다</p>)
     }
 
+    console.log('데이터: ', contentInfo);
+
+<<<<<<< HEAD
+    // 시간 처리
+    const totalTime = contentInfo.createdDate; // 2024-11-05T16:55:18.178236
+    const day = totalTime.split('T')[0];
+    const time = totalTime.split('T')[1].split('.')[0];
+
   return (
     <div>
         <div>
@@ -32,7 +47,7 @@ const ContentsViewer = ({props}) => {
             <h1>글 제목: {contentInfo.title}</h1>
             <div>
                 <p>작성자: {contentInfo.nickname}</p>
-                <p>시간: {modifiedTime} </p>
+                <p>시간: {day} {time} </p>
                 <p>조회수: {contentInfo.hits}</p>
             </div>
         </div>
@@ -48,6 +63,31 @@ const ContentsViewer = ({props}) => {
         <HeartButton/>
     </div>
   );
+=======
+    return (
+        <>
+            <div>
+
+                <h2>카테고리: {contentInfo.category}</h2>
+                <h1>글 제목: {contentInfo.title}</h1>
+                <div>
+                    <p>작성자: {contentInfo.nickname}</p>
+                    <p>시간: {contentInfo.createdDate}</p>
+                    <p>조회수: {contentInfo.hits}</p>
+                </div>
+            </div>
+            <hr/>
+            <div>
+                <Viewer
+                    initialValue={contentInfo.contents}
+                />
+
+            </div>
+            <hr/>
+            <Comments/>
+        </>
+    );
+>>>>>>> b8d70a19c06c054c3dce51058c651bf24c654e0d
 };
 
 export default ContentsViewer;
